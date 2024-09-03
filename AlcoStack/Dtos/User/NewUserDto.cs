03e8a27f49fce8cc9298e030f5bc2170f18adffc
DmytroFlakac <117589874+DmytroFlakac@users.noreply.github.com>
2024-09-03 15:21:24 +0200

    Repository fix.

---
+++ AlcoStack/Dtos/User/NewUserDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AlcoStack.Enums;
+using AlcoStack.Models;
+
+namespace AlcoStack.Dtos;
+
+public class NewUserDto
+{
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Token { get; set; }
+    
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+    
+    public Gender Gender { get; set; } = Gender.Other;
+    public DateOnly? DateOfBirth { get; set; }      
+    public AddressDto? Address { get; set; }  
+    public string? Phone { get; set; }  
+    public string? Photo { get; set; }
+    
+    public string? FormBackgroundUrl { get; set; }
+    public string? Bio { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; } 
+}
---
+++ AlcoStack/Dtos/User/UpdateUserDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AlcoStack.Enums;
+using AlcoStack.Models;
+using Newtonsoft.Json;
+
+namespace AlcoStack.Dtos;
+
+public class UpdateUserDto
+{
+    public string? Username { get; set; }
+    
+    [EmailAddress]
+    public string? Email { get; set; }
+    public string? FirstName { get; set; }   
+
+    public string? LastName { get; set; }
+    
+    public Gender Gender { get; set; } = Gender.Other;
+    public DateOnly? DateOfBirth { get; set; }   
+
+    public AddressDto? Address { get; set; }  
+
+    public string? PhoneNumber { get; set; }  
+    
+    public string? Bio { get; set; }
+   
+    public DateTime UpdatedDate { get; set; } = DateTime.Now;
+}
