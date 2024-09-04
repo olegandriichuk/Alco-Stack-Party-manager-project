1f2a7b954a3e0d7ecc854ebb4fe6c77a67356d62
DmytroFlakac <117589874+DmytroFlakac@users.noreply.github.com>
2024-09-05 00:52:35 +0200

    Seeding alcohol.

--- AlcoStack/Dtos/Alcohol/AlcoholDto.cs
+++ AlcoStack/Dtos/Alcohol/AlcoholDto.cs
@@ -5,14 +5,13 @@ namespace AlcoStack.Dtos;
 public class AlcoholDto
 {
     public Guid Id { get; set; }
-    
+
     public string Name { get; set; }
-    
+
     public AlcoType Type { get; set; }
-    
+
     public string Photo { get; set; }
-    
+
     public string Description { get; set; }
-    
-    public int Rating { get; set; }
+
 }
